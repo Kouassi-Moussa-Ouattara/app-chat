639e85c77df9b0f52abbb402d2b60a8ee393edfb
Kouassi Moussa Ouattara <kouassimoussa.ouatt@gmail.com>
2025-10-07 22:57:36 +0000

    Ajouté BACKEND, FRONTEND et Dockerfile

--- BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
+++ BACKEND/obj/Debug/net9.0/BACKEND.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6ca38a030c21785fad96ba4cb18f799565d0bfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e884ecc0d89e6ed658fdb911867d0f420bed993")]
 [assembly: System.Reflection.AssemblyProductAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BACKEND")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
