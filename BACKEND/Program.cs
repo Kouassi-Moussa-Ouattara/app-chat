5b78b47f42c72e60639a1a0c9e872caff8acc6be
Kouassi Moussa Ouattara <kouassimoussa.ouatt@gmail.com>
2025-10-08 20:23:18 +0000

    Ajouté BACKEND, FRONTEND et Dockerfile

--- BACKEND/Program.cs
+++ BACKEND/Program.cs
@@ -93,7 +93,7 @@ app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowCredentials()
     dbContext.Database.Migrate();
 }*/
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "4200";
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
 app.Urls.Add($"http://0.0.0.0:{port}");
 
 app.UseHttpsRedirection();
